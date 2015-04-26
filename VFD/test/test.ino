a2c6936187ca70c8bc5d667ff2ed87561b9e6fb2
Alkorin <github@alkorin.fr>
2015-04-26 23:43:09 +0200

    Change test code to use Buffer and Serial as input

--- VFD/test/test.ino
+++ VFD/test/test.ino
@@ -1,19 +1,15 @@
 #include "Arduino.h"
-#include "vfd.h"
+#include "vfd_buffered.h"
 
-VFD vfd(0, 1, 3);
+VFD_Buffered vfd(0, 1, 3);
 
 void setup() {
-  vfd.init();
 }
 
 void loop(void) {
-  static int i = 0;
-  i++;
-  
-  char buff[21];
-  sniprintf(buff, 20, "%d \x1a", i);
-  
-  vfd.cursorHome();
-  vfd.writeString(buff);
+  while(Serial.available())
+  {
+    vfd.writeChar(Serial.read());
+  }
+  vfd.show();
 }
